6fb6d4d5b1f5ca4ca5fc736d4e2b691abd13502e
Nghia Pham <nghiaphamsg@gmail.com>
2021-02-14 23:38:21 +0900

    Add simple class socket client

---
+++ inc/WebSocketClient.h
@@ -0,0 +1,79 @@
+/* @brief: Implement a simple WebSocketClient class that encapsulates
+ *         the functionality to connect to a WebSocket server and send
+ *         messages to it.
+ */
+
+#ifndef WEBSOCKET_CLIENT_H
+#define WEBSOCKET_CLIENT_H
+
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
+#include <boost/system/error_code.hpp>
+
+#include <string>
+#include <functional>
+
+namespace NetworkMonitor
+{
+    class WebSocketClient
+    {
+    public:
+        WebSocketClient(
+            const std::string& url,
+            const std::string& port,
+            boost::asio::io_context& ioc
+        );
+        ~WebSocketClient();
+
+        void Connect (
+            std::function<void (boost::system::error_code)> onConnect = nullptr,
+            std::function<void (boost::system::error_code, std::string&&)> onMessage = nullptr,
+            std::function<void (boost::system::error_code)> onDisconnect = nullptr
+        );
+
+        void Send (
+            const std::string& message,
+            std::function<void (boost::system::error_code)> onSend = nullptr
+        );
+
+        void Close (
+            std::function<void (boost::system::error_code)> onClose = nullptr
+        );
+
+    private:
+        std::string url_ {};
+        std::string port_ {};
+
+        boost::asio::ip::tcp::resolver resolver_;
+        boost::beast::websocket::stream<boost::beast::tcp_stream> ws_;
+        boost::beast::flat_buffer rBuffer_;
+
+        std::function<void (boost::system::error_code)> onConnect_ {nullptr};
+        std::function<void (boost::system::error_code, std::string&&)> onMessage_ {nullptr};
+        std::function<void (boost::system::error_code)> onDisconnect_ {nullptr};
+
+        void OnResolve (
+            const boost::system::error_code& ec,
+            boost::asio::ip::tcp::resolver::iterator endpoint
+        );
+
+        void OnConnect (
+            const boost::system::error_code& ec
+        );
+
+        void OnHandshake (
+            const boost::system::error_code& ec
+        );
+
+        void ListenToIncomingMessage (
+            const boost::system::error_code& ec
+        );
+
+        void OnRead (
+            const boost::system::error_code& ec,
+            size_t nBytes
+        );
+    };
+}   /* namespace NetworkMonitor */
+
+#endif  /* WEBSOCKET_CLIENT_H */
---
+++ tests/websocket-client.cpp
@@ -0,0 +1,83 @@
+/* @brief: we provide you with a simple test to verify websocket-client code works
+ * 
+ */
+
+#include "WebSocketClient.h"
+
+#include <boost/asio.hpp>
+
+#include <iostream>
+#include <string>
+
+using NetworkMonitor::WebSocketClient;
+
+int main()
+{
+   /* Connection targets */
+   const std::string url {"echo.websocket.org"};
+   const std::string port {"80"};
+   const std::string message {"Hello WebSocket"};
+
+   /* Always start with an I/O context object */
+   boost::asio::io_context ioc {};
+
+   /* The class under test */
+   WebSocketClient client {url, port, ioc};
+
+   /* We use these flags to check that the connection, send, receive functions
+      work as expected */
+   bool connected {false};
+   bool messageSent {false};
+   bool messageReceived {false};
+   bool messageMatches {false};
+   bool disconnected {false};
+
+   /* Our own callbacks */
+   auto onSend{[&messageSent](auto ec) {
+      messageSent = !ec;
+   }};
+
+   auto onConnect{[&client, &connected, &onSend, &message](auto ec) {
+      connected = !ec;
+      if (!ec)
+      {
+         client.Send(message, onSend);
+      }
+   }};
+
+   auto onClose{[&disconnected](auto ec) {
+      disconnected = !ec;
+   }};
+
+   auto onReceive{[&client,
+                   &onClose,
+                   &messageReceived,
+                   &messageMatches,
+                   &message](auto ec, auto received) {
+      messageReceived = !ec;
+      messageMatches = message == received;
+      client.Close(onClose);
+   }};
+
+   /* We must call io_context::run for asynchronous callbacks to run */
+   client.Connect(onConnect, onReceive);
+   ioc.run();
+
+   /* When we get here, the io_context::run function has run out of work to do */
+   bool ok{
+      connected &&
+      messageSent &&
+      messageReceived &&
+      messageMatches &&
+      disconnected};
+   if (ok)
+   {
+      std::cout << "OK" << std::endl;
+      return 0;
+   }
+   else
+   {
+      std::cerr << "Test failed" << std::endl;
+      return 1;
+   }
+}
